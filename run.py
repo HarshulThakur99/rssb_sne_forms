c1e1081c9db41a994ca629818b9b471a1959bf13
HarshulThakur99 <HarshulThakur99@gmail.com>
2025-10-24 23:26:50 +0530

    blood camp changes

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-# This is now app.py
 import os
-from app import create_app # This import still works correctly
+from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    # These settings are for local development (python app.py)
-    # Gunicorn will use its own settings from the command line
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='127.0.0.1', port=port, debug=True)
